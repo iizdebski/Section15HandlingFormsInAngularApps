d9accfaef27e7ccbdbda33b26ace23bac14f3467
ivan <iizdepskyy112@gmail.com>
2019-10-17 21:26:41 +0300

    188. TD: Adding Validation to check User input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,17 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('f', { static: false }) signupForm: NgForm;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  // onSubmit(form: NgForm) {
-  //  console.log(form);
-  // }
+  // onSubmit(form: NgForm){
+  // console.log(form);
+ // }
 
-  onSubmit() {
-    console.log(this.signupForm);
+ onSubmit() {
+  console.log(this.signupForm);
   }
 }
